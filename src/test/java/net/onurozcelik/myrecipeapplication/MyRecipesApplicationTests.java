cf256946ad60426ee1cec8052af22e4023bc7f83
Onur Ozcelik <muhendis.onur@gmail.com>
2020-06-17 08:34:05 +0300

    First commit

---
+++ src/test/java/net/onurozcelik/myrecipeapplication/MyRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.onurozcelik.myrecipeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
