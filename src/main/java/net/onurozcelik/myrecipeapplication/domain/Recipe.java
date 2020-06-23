2d873d1d0c210ab5fa2ab7f884193d9d2578ef9c
onurozcelik <muhendis.onur@gmail.com>
2020-06-23 21:42:39 +0300

    Added Category entity and associate it with Recipe entity.

--- src/main/java/net/onurozcelik/myrecipeapplication/domain/Recipe.java
+++ src/main/java/net/onurozcelik/myrecipeapplication/domain/Recipe.java
@@ -33,6 +33,11 @@ public class Recipe {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id")
+    , inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     public Long getId() {
         return id;
     }
@@ -111,5 +116,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
