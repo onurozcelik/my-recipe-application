7f2fbc151124fee8d4f6bcc4880da064cc8195b5
onurozcelik <muhendis.onur@gmail.com>
2020-07-04 09:34:41 +0300

    Added more unit tests and some integration tests.

---
+++ src/test/java/net/onurozcelik/myrecipeapplication/domain/UnitOfMeasureTest.java
@@ -0,0 +1,19 @@
+package net.onurozcelik.myrecipeapplication.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+public class UnitOfMeasureTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getDescription() {
+    }
+}
